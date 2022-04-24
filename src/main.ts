1f8dc894c65482d9b9e4fecf59b273b1fd00909e
Emmanuel Nipal <emnnipal@gmail.com>
2022-04-24 21:30:33 +0800

    chore: update package json with production setup

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { AppModule } from './app.module';
 
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
-bootstrap();
+
+bootstrap().catch((err: Error) => {
+  new Logger().log(err);
+});
