67915ac9e038f7413e1bf0bff132b7f7af02355d
Emmanuel Nipal <emnnipal@gmail.com>
2022-04-24 20:12:54 +0800

    feat: initial commit for nest js

---
+++ test/app.e2e-spec.ts
@@ -0,0 +1,24 @@
+import { AppModule } from '../src/app.module';
+
+import { INestApplication } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import * as request from 'supertest';
+
+describe('AppController (e2e)', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('/ (GET)', async () => {
+    const { text } = await request(app.getHttpServer()).get('/').expect(200);
+
+    expect(text).toBe('Hello World!');
+  });
+});
